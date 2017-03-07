324441ba1ef5470800f3a8ceea559fac0927813a
Ralph <sleepwalkinggeek@gmail.com>
2017-03-07 11:30:41 -0600

    Add project files.

---
+++ RalphStore/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using RalphStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RalphStore.Controllers
+{
+    public class StudentController : Controller
+    {
+        private List<StudentModel> students = new List<StudentModel>();
+        // GET: Student
+        public ActionResult Index(string id)
+        {
+            if(students.Count == 0)
+            {
+                students.Add(new StudentModel { ID = 1, FirstName = "Ralph", LastName = "Comb", FavoriteFood = "Coffee" });
+                students.Add(new StudentModel { ID = 2, FirstName = "Jinseong", LastName = "Kim", FavoriteFood = "Pasta" });
+                students.Add(new StudentModel { ID = 3, FirstName = "Sam", LastName = "Fessler", FavoriteFood = "Shrimp" });
+                students.Add(new StudentModel { ID = 4, FirstName = "Erica", LastName = "Wasilenko", FavoriteFood = "Hummus" });
+                students.Add(new StudentModel { ID = 5, FirstName = "Will", LastName = "Mabrey", FavoriteFood = "Ice Cream" });
+                students.Add(new StudentModel { ID = 6, FirstName = "Joe", LastName = "Johnson", FavoriteFood = "Nacho" });
+            }
+
+            return View(students);
+        }
+        public ActionResult GetMoreCoffee()
+        {
+            return Json("I'm going to get more coffee", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
